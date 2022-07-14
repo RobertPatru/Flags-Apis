48d32c909b0ad1316e71ac5f6e123970e6e89841
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-07-14 10:07:22 +0300

    light mode / dark mode togler

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
     let darkTheme = true;
+    let [themeMode, seThemeText] = useState('Dark Mode');
 
     function switchTheme() {
         const root = document.querySelector(':root');
-        
+
         if (darkTheme) {
             darkTheme = false;
+            seThemeText('Light Mode');
             
-        } else {
+            root.style.setProperty('--background-color', 'hsl(0, 0%, 98%)');
+            root.style.setProperty('--secondary-background-color', 'hsl(0, 0%, 100%)');
+            root.style.setProperty('--text-color', 'hsl(200, 15%, 8%)');
+        } 
+        else if(!darkTheme) {
             darkTheme = true;
+            seThemeText('Dark Mode');
+
+            root.style.setProperty('--background-color', 'hsl(207, 26%, 17%)');
+            root.style.setProperty('--secondary-background-color', 'hsl(209, 23%, 22%)');
+            root.style.setProperty('--text-color', 'hsl(0, 0%, 100%)');
         }
 
         console.log(darkTheme);
@@ -21,7 +32,7 @@ function Header() {
         <header className="aling-items-flex row-space-between ">
             <span>Where in the world?</span>
             <button onClick={switchTheme}>
-            <ion-icon name="moon-outline"></ion-icon> Dark Mode
+            <ion-icon name="moon-outline"></ion-icon> {themeMode}
             </button> 
         </header>
     )
