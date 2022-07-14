cb1ba0a46e62b0ed061038071b4484c0684c8ceb
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-07-14 09:49:29 +0300

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
 function Header() {
+    let darkTheme = true;
+
+    function switchTheme() {
+        const root = document.querySelector(':root');
+        
+        if (darkTheme) {
+            darkTheme = false;
+            
+        } else {
+            darkTheme = true;
+        }
+
+        console.log(darkTheme);
+    }
+
+
     return (
         <header className="aling-items-flex row-space-between ">
             <span>Where in the world?</span>
-            <button>
+            <button onClick={switchTheme}>
             <ion-icon name="moon-outline"></ion-icon> Dark Mode
             </button> 
         </header>
