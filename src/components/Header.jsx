2b20303f40dec29a09ecd54a098d9dd03a6b5060
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-07-14 10:25:09 +0300

    light mode/dark mode improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,34 @@
 import React, { useState } from "react";
 
 function Header() {
-    let darkTheme = true;
-    let [themeMode, seThemeText] = useState('Dark Mode');
+    let [darkTheme, setDarkTheme] = useState(true);
+    let [themeModeText, seThemeText] = useState('Dark Mode');
+    let [modeIcon, setModeIcon] = useState(<ion-icon name="moon-outline"></ion-icon>);
 
     function switchTheme() {
         const root = document.querySelector(':root');
 
         if (darkTheme) {
-            darkTheme = false;
+            console.log('Dark mode OFF');
             seThemeText('Light Mode');
+            setModeIcon(<ion-icon name="sunny-outline"></ion-icon>);
             
             root.style.setProperty('--background-color', 'hsl(0, 0%, 98%)');
             root.style.setProperty('--secondary-background-color', 'hsl(0, 0%, 100%)');
             root.style.setProperty('--text-color', 'hsl(200, 15%, 8%)');
+
+            setDarkTheme(false);
         } 
         else if(!darkTheme) {
-            darkTheme = true;
+            console.log('Light mode OFF');
             seThemeText('Dark Mode');
-
+            setModeIcon(<ion-icon name="moon-outline"></ion-icon>);
+            
             root.style.setProperty('--background-color', 'hsl(207, 26%, 17%)');
             root.style.setProperty('--secondary-background-color', 'hsl(209, 23%, 22%)');
             root.style.setProperty('--text-color', 'hsl(0, 0%, 100%)');
+            
+            setDarkTheme(true);
         }
 
         console.log(darkTheme);
@@ -32,7 +39,7 @@ function Header() {
         <header className="aling-items-flex row-space-between ">
             <span>Where in the world?</span>
             <button onClick={switchTheme}>
-            <ion-icon name="moon-outline"></ion-icon> {themeMode}
+            {modeIcon} {themeModeText}
             </button> 
         </header>
     )
